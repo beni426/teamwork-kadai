dedb6324166ee8169415f9ee8358c3d93dfb90b1
Ryu <goskn88006@yahoo.co.jp>
2022-03-08 10:26:59 +0900

    削除ボタンを作成

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -20,6 +20,13 @@ class AgendasController < ApplicationController
       render :new
     end
   end
+  def destroy
+    @agenda = Agenda.find(params[:id])
+    if current_user.id == @agenda.user.id || current_user.id == @team.owner.id
+      @agenda.destroy
+      redirect_to dashboard_url, notice: "#{@agenda.title}を削除しました。"
+    end
+  end
 
   private
 
